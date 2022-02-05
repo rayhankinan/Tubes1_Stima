13a37c4fb3eb438917e8e67c3e9acf0c1eb3e748
Aira Thalca <13520101@std.stei.itb.ac.id>
2022-02-06 03:37:33 +0700

    nambahin command yg blm ada di src

--- src/main/java/za/co/entelect/challenge/Bot.java
+++ src/main/java/za/co/entelect/challenge/Bot.java
@@ -2,50 +2,84 @@ package za.co.entelect.challenge;
 
 import za.co.entelect.challenge.command.*;
 import za.co.entelect.challenge.entities.*;
+import za.co.entelect.challenge.enums.PowerUps;
 import za.co.entelect.challenge.enums.Terrain;
 
 import java.util.*;
 
 import static java.lang.Math.max;
 
+import java.security.SecureRandom;
+
 public class Bot {
 
     private static final int maxSpeed = 9;
-    private List<Integer> directionList = new ArrayList<>();
+    private List<Command> directionList = new ArrayList<>();
 
-    private Random random;
-    private GameState gameState;
-    private Car opponent;
-    private Car myCar;
+    private final Random random;
+
+    private final static Command ACCELERATE = new AccelerateCommand();
+    private final static Command LIZARD = new LizardCommand();
+    private final static Command OIL = new OilCommand();
+    private final static Command BOOST = new BoostCommand();
+    private final static Command EMP = new EmpCommand();
     private final static Command FIX = new FixCommand();
 
-    public Bot(Random random, GameState gameState) {
-        this.random = random;
-        this.gameState = gameState;
-        this.myCar = gameState.player;
-        this.opponent = gameState.opponent;
+    private final static Command TURN_RIGHT = new ChangeLaneCommand(1);
+    private final static Command TURN_LEFT = new ChangeLaneCommand(-1);
 
-        directionList.add(-1);
-        directionList.add(1);
+    public Bot() {
+        this.random = new SecureRandom();
+        directionList.add(TURN_LEFT);
+        directionList.add(TURN_RIGHT);
     }
 
-    public Command run() {
-        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block);
+    public Command run(GameState gameState) {
+        Car myCar = gameState.player;
+        Car opponent = gameState.opponent;
+
+        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
+        List<Object> nextBlocks = blocks.subList(0,1);
+
         if (myCar.damage >= 5) {
-            return new FixCommand();
+            return FIX;
         }
-        if (blocks.contains(Terrain.MUD)) {
-            int i = random.nextInt(directionList.size());
-            return new ChangeLaneCommand(directionList.get(i));
+
+        if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+            if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
+                return LIZARD;
+            }
         }
-        return new AccelerateCommand();
+
+        if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
+            return BOOST;
+        }
+        if (myCar.speed == maxSpeed) {
+            if (hasPowerUp(PowerUps.OIL, myCar.powerups)) {
+                return OIL;
+            }
+            if (hasPowerUp(PowerUps.EMP, myCar.powerups)) {
+                return EMP;
+            }
+        }
+
+        return ACCELERATE;
+    }
+
+    private Boolean hasPowerUp(PowerUps powerUpToCheck, PowerUps[] available) {
+        for (PowerUps powerUp: available) {
+            if (powerUp.equals(powerUpToCheck)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
      * Returns map of blocks and the objects in the for the current lanes, returns the amount of blocks that can be
      * traversed at max speed.
      **/
-    private List<Object> getBlocksInFront(int lane, int block) {
+    private List<Object> getBlocksInFront(int lane, int block, GameState gameState) {
         List<Lane[]> map = gameState.lanes;
         List<Object> blocks = new ArrayList<>();
         int startBlock = map.get(0)[0].position.block;
